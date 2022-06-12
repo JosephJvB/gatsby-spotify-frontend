fb5e8c2b77c461b91602660eb6121c205c95e753
joevanbo <joevanbo@pm.me>
2022-06-12 12:50:41 +1200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
-echo "aws s3 cp ~/WORKSPACE/joe/spotty/frontend/public/"
+echo "aws s3 cp ./public/"
 echo "s3://jaf-unwrapped.site/ --recursive"
-echo "--profile joe"
 echo "aws cloudfront create-invalidation"
 echo "distribution-id EPRLI4PQUKWBM"
 echo "--paths /*"
 echo ""
 echo "------"
 echo ""
-aws s3 cp ~/WORKSPACE/joe/spotty/frontend/public/ \
+aws s3 cp ./public/ \
 s3://jaf-unwrapped.site/ --recursive \
---profile joe
 aws cloudfront create-invalidation \
 --distribution-id EPRLI4PQUKWBM \
 --paths "/*" \
---profile joe
