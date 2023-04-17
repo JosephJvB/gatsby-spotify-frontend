1c5da4a827ee03a4b5f4607f746ef680f3f42158
Joe van Bohemen <joevanbo@protonmail.com>
2023-04-17 09:46:52 +0100

    chores

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,24 +1,24 @@
-import React from "react"
-import HttpClient from "./src/clients/httpClient"
-import { IServiceContext } from "./src/models/context"
-import AdminService from "./src/services/adminService"
-import AuthService from "./src/services/authService"
-import QuizService from "./src/services/quizService"
-import SpotifyService from "./src/services/spotifyService"
+import React from "react";
+import HttpClient from "./src/clients/httpClient";
+import { IServiceContext } from "./src/models/context";
+import AdminService from "./src/services/adminService";
+import AuthService from "./src/services/authService";
+import QuizService from "./src/services/quizService";
+import SpotifyService from "./src/services/spotifyService";
 
-const httpClient = new HttpClient()
+const httpClient = new HttpClient();
 const services: IServiceContext = {
   authService: new AuthService(httpClient),
   quizService: new QuizService(httpClient),
   spotifyService: new SpotifyService(httpClient),
   adminService: new AdminService(httpClient),
-}
-export const ServiceContext = React.createContext(services)
+};
+export const ServiceContext = React.createContext(services);
 
-export const wrapRootElement = ({ element }) => {
+export const wrapRootElement = ({ element }: any) => {
   return (
     <ServiceContext.Provider value={services}>
       {element}
     </ServiceContext.Provider>
-  )
-}
+  );
+};
